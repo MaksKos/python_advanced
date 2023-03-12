25dd426cc3d8117dbe5f9336ddfc20aeb6254af9
MaksKos <maks.kostrow@yandex.ru>
2023-03-12 22:34:46 +0300

    make files

---
+++ hw_1/mess_eval.py
@@ -0,0 +1,12 @@
+class SomeModel:
+    def predict(self, message: str) -> float:
+        pass
+
+
+def predict_message_mood(
+    message: str,
+    model: SomeModel,
+    bad_thresholds: float = 0.3,
+    good_thresholds: float = 0.8,
+) -> str:
+    pass
