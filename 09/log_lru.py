e3557b1fa7651b78640c5ee09b8ba80d19a0f683
MaksKos <maks.kostrow@yandex.ru>
2023-05-26 12:55:46 +0300

    fix mistake

--- 09/log_lru.py
+++ 09/log_lru.py
@@ -3,23 +3,42 @@
 import logging
 import argparse
 
-loglru = logging.getLogger('loglru')
 
-format_file = logging.Formatter("%(asctime)s\t%(levelname)s\t[file]\t%(message)s",
-                                "%Y %b %d, %H:%M:%S")
-format_stdout = logging.Formatter("%(asctime)s\t%(levelname)s\t%(message)s",
-                                  "%H:%M:%S")
+def logger_config(filt, std):
 
-hand_file = logging.FileHandler("cache.log", encoding='utf-8')
-hand_file.setLevel(logging.DEBUG)
-hand_file.setFormatter(format_file)
+    loglru = logging.getLogger('loglru')
 
-hand_std = logging.StreamHandler()
-hand_std.setLevel(logging.WARNING)
-hand_std.setFormatter(format_stdout)
+    format_file = logging.Formatter("%(asctime)s\t%(levelname)s\t[file]\t%(message)s",
+                                    "%Y %b %d, %H:%M:%S")
+    format_stdout = logging.Formatter("%(asctime)s\t%(levelname)s\t%(message)s",
+                                      "%H:%M:%S")
 
-loglru.setLevel(logging.DEBUG)
-loglru.addHandler(hand_file)
+    hand_file = logging.FileHandler("cache.log", encoding='utf-8')
+    hand_file.setLevel(logging.DEBUG)
+    hand_file.setFormatter(format_file)
+
+    hand_std = logging.StreamHandler()
+    hand_std.setLevel(logging.WARNING)
+    hand_std.setFormatter(format_stdout)
+
+    loglru.setLevel(logging.DEBUG)
+    loglru.addHandler(hand_file)
+
+    if filt:
+        loglru.addFilter(NodeInfoFilter())
+    if std:
+        loglru.addHandler(hand_std)
+
+    return loglru
+
+
+def get_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', action="store_true")
+    parser.add_argument('-f', action="store_true")
+    args = parser.parse_args()
+    return args
 
 
 class NodeInfoFilter(logging.Filter):
@@ -31,7 +50,7 @@ class NodeInfoFilter(logging.Filter):
 
 class Node:
 
-    def __init__(self, key, value):
+    def __init__(self, key, value, loglru):
         self.key = key
         self.value = value
         self.prev = None
@@ -41,15 +60,16 @@ class Node:
 
 class LRUCache:
 
-    def __init__(self, limit=42):
+    def __init__(self, logger, limit=42):
         self.cache = dict()
         self.head = None
         self.tail = None
         self.limit = limit
-        loglru.info('create lru with size %d', limit)
+        self.loglru = logger
+        self.loglru.info('create lru with size %d', limit)
 
     def _appnode(self, new_node):
-        loglru.debug('add node with key <%s>', new_node.key)
+        self.loglru.debug('add node with key <%s>', new_node.key)
         if self.head is None:
             self.head = new_node
             self.tail = new_node
@@ -60,7 +80,7 @@ class LRUCache:
             self.head = new_node
 
     def _removenode(self, node):
-        loglru.debug('remove node with key <%s>', node.key)
+        self.loglru.debug('remove node with key <%s>', node.key)
         node_next = node.next
         node_prev = node.prev
         if node is self.head and node is self.tail:
@@ -79,45 +99,43 @@ class LRUCache:
         return None
 
     def _move_to_head(self, node):
-        loglru.debug('move to head node with key <%s>', node.key)
+        self.loglru.debug('move to head node with key <%s>', node.key)
         self._removenode(node)
         self._appnode(node)
 
     def get(self, key):
         node = self.cache.get(key)
         if node is None:
-            loglru.critical('get: key <%s> not found', key)
+            self.loglru.critical('get: key <%s> not found', key)
             return None
-        loglru.info('get: key <%s> is found', key)
+        self.loglru.info('get: key <%s> is found', key)
         self._move_to_head(node)
         return node.value
 
     def set(self, key, value):
         node = self.cache.get(key)
         if node is None:
-            loglru.warning('set: add new key <%s>', key)
-            new_node = Node(key, value)
+            self.loglru.warning('set: add new key <%s>', key)
+            new_node = Node(key, value, self.loglru)
             self.cache[key] = new_node
             self._appnode(new_node)
         else:
-            loglru.info('set: update key <%s>', key)
+            self.loglru.info('set: update key <%s>', key)
             node.value = value
             self._move_to_head(node)
 
         if len(self.cache) > self.limit:
-            loglru.error('set: delete key <%s>', self.tail.key)
+            self.loglru.error('set: delete key <%s>', self.tail.key)
             self.cache.pop(self.tail.key)
             self._removenode(self.tail)
 
 
-def main(std: bool, filt: bool):
+def main():
 
-    if filt:
-        loglru.addFilter(NodeInfoFilter())
-    if std:
-        loglru.addHandler(hand_std)
+    args = get_args()
+    logger = logger_config(args.f, args.s)
 
-    lru_cache = LRUCache(3)
+    lru_cache = LRUCache(logger, 3)
 
     lru_cache.set("url1", "news site")
     lru_cache.set("url2", "messanger")
@@ -130,9 +148,4 @@ def main(std: bool, filt: bool):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', action="store_true")
-    parser.add_argument('-f', action="store_true")
-    args = parser.parse_args()
-
-    main(args.s, args.f)
+    main()
